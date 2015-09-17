9623def09230eb3079c1baac775b778a6351db93
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-09-17 23:11:47 +0100

    Added minitest

--- test/controllers/api/v1/bucketlists_controller_test.rb
+++ test/controllers/api/v1/bucketlists_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class Api::V1::BucketlistsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  # end
 
 end
--- test/models/bucketlist_test.rb
+++ test/models/bucketlist_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
-
+require 'pry'
 class BucketlistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "A bucket is valid with a name and a user" do
+    # user = User.new(email: "stephen@yahoo.com", password: "password", name: "stephen")
+    # Binding.pry
+    # user.save
+    #
+    # bucket = Bucketlist.new(name: "bucket", user_id: user.id)
+    # bucket.save
+    #
+    # assert bucket.valid?
+    # refute bucket.invalid?
+  end
 end
